16e9214351b7b5956e6987f44a8299bc6cb2e862
Benedikt Geissler <benedikt@g5r.eu>
2018-05-31 14:39:21 +0200

    Take care of paged API
    In addition to that:
    
    * write to stdout instead of a file
    * allow to export list of followers

--- gnu-social-export.py
+++ gnu-social-export.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 import requests
 from dotenv import load_dotenv
@@ -10,35 +11,49 @@ def main():
         description='Export the list of followed users from GNU social',
     )
     ap.add_argument(
-        '-e', '--envfile',
-        nargs='?',
-        default='.env',
-    )
+        '-e', '--envfile', nargs='?', default='.env',
+        help='path to the environment file')
     ap.add_argument(
-        '-o', '--outfile',
-        nargs='?',
-        default='following_accounts.csv',
-    )
+        '-f', '--followers', action='store_true',
+        help='export list of followers instead of friends')
     args = ap.parse_args()
     load_dotenv(args.envfile)
-    hostname = os.environ['GNU_SOCIAL_HOST']
-    username = os.environ['GNU_SOCIAL_USER']
-    password = os.environ['GNU_SOCIAL_PASS']
-
+    apiroot = os.environ['GNU_SOCIAL_HOST'] + '/api/'
+    credentials = (os.environ['GNU_SOCIAL_USER'],
+                   os.environ['GNU_SOCIAL_PASS'])
+    pagesize = 100
     r = requests.get(
-        '{host}/api/statuses/friends.json'.format(host=hostname),
-        auth=(username, password),
-    )
-    accounts = {
-        '{}@{}\n'.format(
-            account['statusnet_profile_url'].split('/')[-1].lstrip('@'),
-            account['statusnet_profile_url'].split('/')[2],
-        )
-        for account in r.json()
-    }
+        apiroot + 'statuses/user_timeline.json', auth=credentials)
+    t = 'followers' if args.followers is True else 'friends'
+    page_count = r.json()[0]['user'][t + '_count'] // pagesize + 1
+    e_print('will make {} requests'.format(page_count))
 
-    with open(args.outfile, 'w') as f:
-        f.writelines(accounts)
+    if args.followers is True:
+        endpoint = 'statuses/followers.json'
+    else:
+        endpoint = 'statuses/friends.json'
+    accounts = []
+    for i in range(1, page_count + 1):
+        r = requests.get(
+            '{}{}?count={}&page={}'.format(
+                apiroot, endpoint, pagesize, i),
+            auth=credentials)
+        accounts += [
+            '@'.join([
+                account['statusnet_profile_url'].split('/')[-1].lstrip('@'),
+                account['statusnet_profile_url'].split('/')[2],
+            ])
+            for account in r.json()
+        ]
+        e_print('request #{} processed'.format(i))
+
+    e_print('ready')
+    for acc in accounts:
+        print(acc)
+
+
+def e_print(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 if __name__ == '__main__':
